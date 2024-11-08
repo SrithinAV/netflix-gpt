c0337d3d7df7e71bb98a90069fd2b681b131163e
Srithin A V <7srithin@gmail.com>
2024-11-08 11:07:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-yellow-200'> 
+      hello
+
+    </div>
+  )
+}
+
+export default App
